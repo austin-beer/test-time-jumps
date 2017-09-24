132203a4b3cf96b048adf896edeabb4c18ab7a75
Austin Beer <asbeer@gmail.com>
2017-09-24 17:03:43 -0600

    Updating test results with latest in GitHub.

--- run_windows.bat
+++ run_windows.bat
@@ -1,3 +1,3 @@
-    powershell "./test_time_jumps_orig_no_monotonic.exe   | tee windows_results_orig_no_monotonic.txt"
+rem powershell "./test_time_jumps_orig_no_monotonic.exe   | tee windows_results_orig_no_monotonic.txt"
     powershell "./test_time_jumps_fixed_no_monotonic.exe  | tee windows_results_fixed_no_monotonic.txt"
 rem powershell "./test_time_jumps_austin_no_monotonic.exe | tee windows_results_austin_no_monotonic.txt"
