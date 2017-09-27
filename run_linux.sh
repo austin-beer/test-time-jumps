2224074c8566e63a5f4193ef97120078be63f11e
Austin Beer <asbeer@gmail.com>
2017-09-27 10:51:25 -0600

    Updated the Windows makefile to stop on error and to not echo out the commands it's running.

--- run_linux.sh
+++ run_linux.sh
@@ -3,5 +3,5 @@
  ./test_time_jumps_orig_yes_monotonic   | tee linux_results_orig_yes_monotonic.txt
  ./test_time_jumps_fixed_no_monotonic   | tee linux_results_fixed_no_monotonic.txt
  ./test_time_jumps_fixed_yes_monotonic  | tee linux_results_fixed_yes_monotonic.txt
-#./test_time_jumps_austin_no_monotonic  | tee linux_results_austin_no_monotonic.txt
-#./test_time_jumps_austin_yes_monotonic | tee linux_results_austin_yes_monotonic.txt
+ ./test_time_jumps_austin_no_monotonic  | tee linux_results_austin_no_monotonic.txt
+ ./test_time_jumps_austin_yes_monotonic | tee linux_results_austin_yes_monotonic.txt
--- run_windows.bat
+++ run_windows.bat
@@ -2,5 +2,5 @@
     powershell "./test_time_jumps_orig_yes_generic_shared.exe   | tee windows_results_orig_yes_generic_shared.txt"
     powershell "./test_time_jumps_fixed_no_generic_shared.exe   | tee windows_results_fixed_no_generic_shared.txt"
     powershell "./test_time_jumps_fixed_yes_generic_shared.exe  | tee windows_results_fixed_yes_generic_shared.txt"
-rem powershell "./test_time_jumps_austin_no_generic_shared.exe  | tee windows_results_austin_no_generic_shared.txt"
-rem powershell "./test_time_jumps_austin_yes_generic_shared.exe | tee windows_results_austin_yes_generic_shared.txt"
+    powershell "./test_time_jumps_austin_no_generic_shared.exe  | tee windows_results_austin_no_generic_shared.txt"
+    powershell "./test_time_jumps_austin_yes_generic_shared.exe | tee windows_results_austin_yes_generic_shared.txt"
