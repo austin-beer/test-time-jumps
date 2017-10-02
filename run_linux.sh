1efc6a65b8d47d98cbdf5b489659df229202a994
Austin Beer <asbeer@gmail.com>
2017-10-02 17:44:53 -0600

    Updating the results against the latest code on GitHub.

--- run_linux.sh
+++ run_linux.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+systemctl stop ntpd
  ./test_time_jumps_orig_no_monotonic    | tee linux_results_orig_no_monotonic.txt
  ./test_time_jumps_orig_yes_monotonic   | tee linux_results_orig_yes_monotonic.txt
  ./test_time_jumps_fixed_no_monotonic   | tee linux_results_fixed_no_monotonic.txt
  ./test_time_jumps_fixed_yes_monotonic  | tee linux_results_fixed_yes_monotonic.txt
  ./test_time_jumps_austin_no_monotonic  | tee linux_results_austin_no_monotonic.txt
  ./test_time_jumps_austin_yes_monotonic | tee linux_results_austin_yes_monotonic.txt
+systemctl start ntpd
