663bc3b4ee27202ba4085cb01799a42f08c96c5b
Austin Beer <asbeer@gmail.com>
2017-10-12 18:43:10 -0600

    Disabled NTP synchronization on Windows while running tests. Added list of test files that I've edited.

--- run_windows.bat
+++ run_windows.bat
@@ -1,6 +1,8 @@
+    net stop w32time
     powershell "./test_time_jumps_orig_no_generic_shared.exe    | tee windows_results_orig_no_generic_shared.txt"
     powershell "./test_time_jumps_orig_yes_generic_shared.exe   | tee windows_results_orig_yes_generic_shared.txt"
     powershell "./test_time_jumps_fixed_no_generic_shared.exe   | tee windows_results_fixed_no_generic_shared.txt"
     powershell "./test_time_jumps_fixed_yes_generic_shared.exe  | tee windows_results_fixed_yes_generic_shared.txt"
     powershell "./test_time_jumps_austin_no_generic_shared.exe  | tee windows_results_austin_no_generic_shared.txt"
     powershell "./test_time_jumps_austin_yes_generic_shared.exe | tee windows_results_austin_yes_generic_shared.txt"
+    net start w32time
