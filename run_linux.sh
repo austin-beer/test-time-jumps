bf785099789cd4488fbbd52cc47a26454863cec5
Austin Beer <asbeer@gmail.com>
2018-02-16 11:34:23 -0700

    Update makefiles and results after timespec_clocks merged back to develop.

--- run_linux.sh
+++ run_linux.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
- systemctl stop ntpd
- ./test_time_jumps_orig1_default       | tee linux_results_orig1_default.txt
- ./test_time_jumps_orig2_mono_condattr | tee linux_results_orig2_mono_condattr.txt
- ./test_time_jumps_fixed1_default      | tee linux_results_fixed1_default.txt
- ./test_time_jumps_fixed2_v2_shared    | tee linux_results_fixed2_v2_shared.txt
- ./test_time_jumps_austin1_default     | tee linux_results_austin1_default.txt
- ./test_time_jumps_austin2_v2_shared   | tee linux_results_austin2_v2_shared.txt
- systemctl start ntpd
+systemctl stop ntpd
+ ./test_time_jumps_develop1_default   | tee linux_results_develop1_default.txt
+ ./test_time_jumps_develop2_v2_shared | tee linux_results_develop2_v2_shared.txt
+#./test_time_jumps_feature1_default   | tee linux_results_feature1_default.txt
+#./test_time_jumps_feature2_v2_shared | tee linux_results_feature2_v2_shared.txt
+ ./test_time_jumps_austin1_default    | tee linux_results_austin1_default.txt
+ ./test_time_jumps_austin2_v2_shared  | tee linux_results_austin2_v2_shared.txt
+#./git_austin/bin.v2/libs/thread/test/gcc-5.4.0/dbg/thrdp-pthrd/thrd-mlt/test_time_jumps_1 | tee linux_results_test1.txt
+#./git_austin/bin.v2/libs/thread/test/gcc-5.4.0/dbg/thrdp-pthrd/thrd-mlt/test_time_jumps_2 | tee linux_results_test2.txt
+#./git_austin/bin.v2/libs/thread/test/gcc-5.4.0/dbg/thrdp-pthrd/thrd-mlt/test_time_jumps_3 | tee linux_results_test3.txt
+systemctl start ntpd
--- run_windows.bat
+++ run_windows.bat
@@ -1,10 +1,14 @@
-    net stop w32time
-    powershell "./test_time_jumps_orig1_default.exe          | tee windows_results_orig1_default.txt"
-    powershell "./test_time_jumps_orig2_generic_shared.exe   | tee windows_results_orig2_generic_shared.txt"
-    powershell "./test_time_jumps_fixed1_default.exe         | tee windows_results_fixed1_default.txt"
-    powershell "./test_time_jumps_fixed2_generic_shared.exe  | tee windows_results_fixed2_generic_shared.txt"
-    powershell "./test_time_jumps_fixed3_v2_shared.exe       | tee windows_results_fixed3_v2_shared.txt"
-    powershell "./test_time_jumps_austin1_default.exe        | tee windows_results_austin1_default.txt"
-    powershell "./test_time_jumps_austin2_generic_shared.exe | tee windows_results_austin2_generic_shared.txt"
-    powershell "./test_time_jumps_austin3_v2_shared.exe      | tee windows_results_austin3_v2_shared.txt"
-    net start w32time
+net stop w32time
+    powershell ".\test_time_jumps_develop1_default.exe         | tee windows_results_develop1_default.txt"
+    powershell ".\test_time_jumps_develop2_generic_shared.exe  | tee windows_results_develop2_generic_shared.txt"
+    powershell ".\test_time_jumps_develop3_v2_shared.exe       | tee windows_results_develop3_v2_shared.txt"
+rem powershell ".\test_time_jumps_feature1_default.exe         | tee windows_results_feature1_default.txt"
+rem powershell ".\test_time_jumps_feature2_generic_shared.exe  | tee windows_results_feature2_generic_shared.txt"
+rem powershell ".\test_time_jumps_feature3_v2_shared.exe       | tee windows_results_feature3_v2_shared.txt"
+    powershell ".\test_time_jumps_austin1_default.exe          | tee windows_results_austin1_default.txt"
+    powershell ".\test_time_jumps_austin2_generic_shared.exe   | tee windows_results_austin2_generic_shared.txt"
+    powershell ".\test_time_jumps_austin3_v2_shared.exe        | tee windows_results_austin3_v2_shared.txt"
+rem powershell ".\git_austin\bin.v2\libs\thread\test\msvc-14.1\dbg\lnk-sttc\thrdp-wn32\thrd-mlt\test_time_jumps_1.exe | tee windows_results_test1.txt"
+rem powershell ".\git_austin\bin.v2\libs\thread\test\msvc-14.1\dbg\lnk-sttc\thrdp-wn32\thrd-mlt\test_time_jumps_2.exe | tee windows_results_test2.txt"
+rem powershell ".\git_austin\bin.v2\libs\thread\test\msvc-14.1\dbg\lnk-sttc\thrdp-wn32\thrd-mlt\test_time_jumps_3.exe | tee windows_results_test3.txt"
+net start w32time
